773cdf92b61b18e361d9beb4c7e3bbc0e4e87dd6
ibnuumar448 <saminuumar2027@gmail.com>
2024-12-14 09:21:41 +0100

    new file:   06_Module_Dictionaries/dictionaries.ipynb

---
+++ 05_lists_tuples_sets/Untitled-2.py
@@ -0,0 +1,17 @@
+student = dict(Name = "Saminu",
+               Age = 23,
+               Marital_status = "Single",
+               State = "Bauchi",
+               Course = "Comp_Eng")
+print(student)
+
+
+
+
+
+
+
+
+
+num = range(1, 10)
+print(num)
